e097bfcd18520c6e5c2afca90a4d78cca000afc2
Ron Davidson <davron12@hotmail.com>
2018-04-07 22:29:55 -0400

    Added browser support fallback

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -2,10 +2,23 @@ var gulp = require( 'gulp' );
 var svgSprite = require( 'gulp-svg-sprite' );
 var rename = require( 'gulp-rename' );
 var del = require( 'del' );
+var svg2png = require( 'gulp-svg2png' );
 
 var config = {
+  shape: {
+    spacing: {
+      padding: 1
+    }
+  },
   mode: {
     css: {
+      variables: {
+        replaceSvgWithPng: function() {
+          return function( sprite, render ) {
+            return render( sprite ).split( '.svg' ).join( '.png' );
+          }
+        }
+      },
       sprite: 'sprite.svg',
       render: {
         css: {
@@ -16,7 +29,7 @@ var config = {
   }
 };
 
-gulp.task( 'icons', [ 'beginClean', "createSprite", "copySpriteCSS", "copySpriteGraphic", 'endClean' ] );
+gulp.task( 'icons', [ 'beginClean', "createSprite", "createPngCopy", "copySpriteCSS", "copySpriteGraphic", 'endClean' ] );
 
 gulp.task( 'beginClean', function() {
   return del( [ './app/temp/sprite', './app/assets/images/sprites' ] );
@@ -32,8 +45,14 @@ gulp.task( 'createSprite', [ 'beginClean' ], function() {
     .pipe( gulp.dest( './app/temp/sprite/' ) );
 } );
 
-gulp.task( 'copySpriteGraphic', [ 'createSprite' ], function() {
-  return gulp.src( './app/temp/sprite/css/**/*.svg' )
+gulp.task( 'createPngCopy', [ 'createSprite' ], function() {
+  return gulp.src( './app/temp/sprite/css/*.svg' )
+    .pipe( svg2png() )
+    .pipe( gulp.dest( './app/temp/sprite/css' ) );
+} );
+
+gulp.task( 'copySpriteGraphic', [ 'createPngCopy' ], function() {
+  return gulp.src( './app/temp/sprite/css/**/*.{svg,png}' )
     .pipe( gulp.dest( './app/assets/images/sprites' ) );
 } );
 
